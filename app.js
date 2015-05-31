0f6de8fb559ef6230eeffec238eea53a83bf2498
Arash Namvar <namvar.arash@gmail.com>
2015-05-31 09:35:52 -0700

    please4321

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 var express = require("express");
+var session = require('express-session');
 var path = require("path");
 var bodyParser = require("body-parser");
 var app = express();
-app.use(bodyParser.json());
-
 var twilio = require('twilio');
 var qs = require('querystring');
-app.use(express.static(path.join(__dirname, "./client")));
 
-var server = app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "./client")));
 
 var accountSid = 'AC23d38d64f113cbd57fe69b744ae37c46';
 var authToken = '4767a1a13814d3e80b13773824e79f44';
@@ -44,3 +41,7 @@ app.post('/processtext', function(req,res) {
         });
         // res.end('send a POST');
   });
+
+var server = app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
