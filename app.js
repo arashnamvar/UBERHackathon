31bed4f85d70ce090b4c9bd698bb0d902c3c5619
Arash Namvar <namvar.arash@gmail.com>
2015-05-31 08:58:16 -0700

    wah

--- app.js
+++ app.js
@@ -24,61 +24,21 @@ client.messages.create({
     process.stdout.write(message.sid);
 });
 
-
-// app.post('/processtext', function(req,res) {
-//     if (req.method == 'POST') {
-//         var body = '';
-
-//         req.on('data', function (data) {
-//             body += data;
-//         });
-
-//         req.on('end', function () {
-
-//           // console.log(body);
-//             var POST = qs.parse(body);
-//             console.log(POST);
-
-//             if (POST.From == "+14083869581") {
-//                 client.messages.create({
-//                     body: "Hi Arash",
-//                     to: "+14083869581",
-//                     from: "+16505420611"
-//                 }, function(err, message) {
-//                     process.stdout.write(message.sid);
-//                 });
-//             }
-
-//             //validate incoming request is from twilio using your auth token and the header from Twilio
-//             var token = '4767a1a13814d3e80b13773824e79f44',
-//                 header = req.headers['x-twilio-signature'];
-
-//             //validateRequest returns true if the request originated from Twilio
-//             if (twilio.validateRequest(token, header, 'https://uberforall.herokuapp.com', POST)) {
-//                 //generate a TwiML response
-//                 var resp = new twilio.TwimlResponse();
-//                 resp.say('hello, twilio!');
-
-//                 res.writeHead(200, { 'Content-Type':'text/xml' });
-//                 res.end(resp.toString());
-//             }
-//             else {
-//                 res.writeHead(403, { 'Content-Type':'text/plain' });
-//                 res.end('you are not twilio - take a hike.');
-//             }
-//         });
-//     }
-//     else {
-//         res.writeHead(404, { 'Content-Type':'text/plain' });
-//         res.end('send a POST');
-//     }
-//   });
-
-
 app.post('/processtext', function(req,res) {
-        console.log(req.From);
+        var body = '';
+        var post;
 
-            if (POST.From == "+14083869581") {
+        req.on('data', function (data) {
+            body += data;
+        });
+
+        req.on('end', function() {
+            post = qs.parse(body);
+        });
+
+
+        console.log(post);
+
                 client.messages.create({
                     body: "Hi Arash",
                     to: "+14083869581",
@@ -86,5 +46,4 @@ app.post('/processtext', function(req,res) {
                 }, function(err, message) {
                     process.stdout.write(message.sid);
                 });
-            }
   });
