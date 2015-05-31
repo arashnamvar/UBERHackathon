370589fabd985113ca60a24ac901a8eb73c60e3f
Arash Namvar <namvar.arash@gmail.com>
2015-05-31 02:42:23 -0700

    texts everyone, trying to receive correct post data

--- app.js
+++ app.js
@@ -116,29 +116,97 @@ var users = [];
 
 // });
 
-app.post('/processtext', function(req,res) {
-    var inputText = req.body.info;
-    var splitText = req.body.info.split(":");
-    console.log(splitText);
-    var trip = [];
-    geocoder.geocode(splitText[0], function(err, res){
-        if(err){
-            console.log(err);
-        }
-        else{
-            trip.push({ lat: res[0].latitude, long: res[0].longitude });
-        }
-    });
-    geocoder.geocode(splitText[1], function(err, res){
-        if(err){
-            console.log(err);
-        }
-        else{
-            trip.push({ lat: res[0].latitude, long: res[0].longitude });
-        }
-    });
-    console.log(trip);
-});
+pp.post('/processtext', function(req,res) {
+    if (req.method == 'POST') {
+        var body = '';
+
+        req.on('data', function (data) {
+            body += data;
+        });
+
+        req.on('end', function () {
+
+          // console.log(body);
+            var POST = qs.parse(body);
+            console.log(POST);
+
+            if (POST.From == "+14083869581") {
+                client.messages.create({
+                    body: "FUCK OFF NERD",
+                    to: "+14083869581",
+                    from: "+16505420611"
+                }, function(err, message) {
+                    process.stdout.write(message.sid);
+                });
+            }
+
+            if (POST.From == "+15103344759") {
+                client.messages.create({
+                    body: "Hey Uyanga",
+                    to: "+15103344759",
+                    from: "+16505420611"
+                }, function(err, message) {
+                    process.stdout.write(message.sid);
+                });
+            }
+
+            if (POST.From == "+12177214157") {
+                client.messages.create({
+                    body: "Hey dudeman",
+                    to: "+12177214157",
+                    from: "+16505420611"
+                }, function(err, message) {
+                    process.stdout.write(message.sid);
+                });
+            }
+
+            if (POST.From == "+19142635538") {
+                client.messages.create({
+                    body: "Hey hefferson, you lil b",
+                    to: "+19142635538",
+                    from: "+16505420611"
+                }, function(err, message) {
+                    process.stdout.write(message.sid);
+                });
+            }
+
+            if (POST.From == "+16502834692") {
+                client.messages.create({
+                    body: "What up you bitch, Josh",
+                    to: "+16502834692",
+                    from: "+16505420611"
+                }, function(err, message) {
+                    process.stdout.write(message.sid);
+                });
+            }
+
+
+
+
+            //validate incoming request is from twilio using your auth token and the header from Twilio
+            var token = '4767a1a13814d3e80b13773824e79f44',
+                header = req.headers['x-twilio-signature'];
+
+            //validateRequest returns true if the request originated from Twilio
+            if (twilio.validateRequest(token, header, 'https://uberforall.herokuapp.com/', POST)) {
+                //generate a TwiML response
+                var resp = new twilio.TwimlResponse();
+                resp.say('hello, twilio!');
+
+                res.writeHead(200, { 'Content-Type':'text/xml' });
+                res.end(resp.toString());
+            }
+            else {
+                res.writeHead(403, { 'Content-Type':'text/plain' });
+                res.end('you are not twilio - take a hike.');
+            }
+        });
+    }
+    else {
+        res.writeHead(404, { 'Content-Type':'text/plain' });
+        res.end('send a POST');
+    }
+  });
 
 passport.serializeUser(function (user, done){
     done(null, user);
