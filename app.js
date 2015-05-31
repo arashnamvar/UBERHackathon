e28dcd50d63588ea5922f51fe874662f3b2a6800
Arash Namvar <namvar.arash@gmail.com>
2015-05-31 05:14:12 -0700

    arash stuff

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
 var express = require("express");
-require('./config/mongoose.js');
 var path = require("path");
 var app = express();
 var bodyParser = require("body-parser");
-app.use(bodyParser.json());
-
+var accountSid = 'AC23d38d64f113cbd57fe69b744ae37c46';
 var twilio = require('twilio');
 var qs = require('querystring');
+var authToken = '4767a1a13814d3e80b13773824e79f44';
+var client = require('twilio')(accountSid, authToken);
 
-
-// require('./config/routes.js')(app);
-
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "./client")));
 
 var server = app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
-
-var accountSid = 'AC23d38d64f113cbd57fe69b744ae37c46';
-var authToken = '4767a1a13814d3e80b13773824e79f44';
-var client = require('twilio')(accountSid, authToken);
  
 client.messages.create({
     body: "Send me a response, prease",
@@ -28,44 +22,6 @@ client.messages.create({
 }, function(err, message) {
     process.stdout.write(message.sid);
 });
-
-// client.messages.create({
-//     body: "Send me a response, prease",
-//     to: "+15103344759",
-//     from: "+16505420611"
-// }, function(err, message) {
-//     process.stdout.write(message.sid);
-// });
-
-// client.messages.create({
-//     body: "Send me a response, prease",
-//     to: "+12177214157",
-//     from: "+16505420611"
-// }, function(err, message) {
-//     process.stdout.write(message.sid);
-// });
-
-// client.messages.create({
-//     body: "Send me a response, prease",
-//     to: "+19142635538",
-//     from: "+16505420611"
-// }, function(err, message) {
-//     process.stdout.write(message.sid);
-// });
-
-// client.messages.create({
-//     body: "Send me a response, prease",
-//     to: "+16502834692",
-//     from: "+16505420611"
-// }, function(err, message) {
-//     process.stdout.write(message.sid);
-// });
-
-
-
-
-
-
 
 // var sys = require('sys');
 
@@ -93,8 +49,6 @@ app.post('/processtext', function(req,res) {
         });
 
         req.on('end', function () {
-
-          // console.log(body);
             var POST = qs.parse(body);
             console.log(POST);
 
@@ -107,48 +61,6 @@ app.post('/processtext', function(req,res) {
                     process.stdout.write(message.sid);
                 });
             }
-
-            // if (POST.From == "+15103344759") {
-            //     client.messages.create({
-            //         body: "Hey Uyanga",
-            //         to: "+15103344759",
-            //         from: "+16505420611"
-            //     }, function(err, message) {
-            //         process.stdout.write(message.sid);
-            //     });
-            // }
-
-            // if (POST.From == "+12177214157") {
-            //     client.messages.create({
-            //         body: "Hey dudeman",
-            //         to: "+12177214157",
-            //         from: "+16505420611"
-            //     }, function(err, message) {
-            //         process.stdout.write(message.sid);
-            //     });
-            // }
-
-            // if (POST.From == "+19142635538") {
-            //     client.messages.create({
-            //         body: "Hey hefferson, you lil b",
-            //         to: "+19142635538",
-            //         from: "+16505420611"
-            //     }, function(err, message) {
-            //         process.stdout.write(message.sid);
-            //     });
-            // }
-
-            // if (POST.From == "+16502834692") {
-            //     client.messages.create({
-            //         body: "What up you bitch, Josh",
-            //         to: "+16502834692",
-            //         from: "+16505420611"
-            //     }, function(err, message) {
-            //         process.stdout.write(message.sid);
-            //     });
-            // }
-
-            //validate incoming request is from twilio using your auth token and the header from Twilio
             var token = '4767a1a13814d3e80b13773824e79f44',
                 header = req.headers['x-twilio-signature'];
 
@@ -160,6 +72,8 @@ app.post('/processtext', function(req,res) {
 
                 res.writeHead(200, { 'Content-Type':'text/xml' });
                 res.end(resp.toString());
+
+                // PUT JEFF's CODE
             }
             else {
                 res.writeHead(403, { 'Content-Type':'text/plain' });
