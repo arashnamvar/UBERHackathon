4acf28dd2eb3eb910c3f1daf18722940e6ab8bf7
Arash Namvar <namvar.arash@gmail.com>
2015-05-31 00:35:02 -0700

    Jeff + Arash's Code

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	ClientID: "BPHhXK015dMNta8CsF5KMXOM5_7sCW8B",
+	ClientSecret: "8DHl66OrfsrFl1pprHnt8IICI2ZXfDUy-cllKYBg",
+	ServerID: "Of5i2Gv2Nb3hWAULccDeBP-eBlVSDcEWCmroEIxd"
+};
