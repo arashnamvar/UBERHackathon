0f93be89180026e6e5044df75f465a2577b386ef
Arash Namvar <namvar.arash@gmail.com>
2015-05-30 21:26:32 -0700

    old text to arash

--- app.js
+++ app.js
@@ -22,12 +22,29 @@ var authToken = '4767a1a13814d3e80b13773824e79f44';
 var client = require('twilio')(accountSid, authToken);
  
 client.messages.create({
-    body: "Hi Josh",
-    to: "+16502834692",
+    body: "Jenny please?! I love you <3",
+    to: "+14083869581",
     from: "+16505420611"
 }, function(err, message) {
     process.stdout.write(message.sid);
 });
+
+// var sys = require('sys');
+
+// app.post('http://f8ec7b1b.ngrok.io', function(req, res) {
+
+// var message = req.body.Body;
+// var from = req.body.From;
+// sys.log('From: ' + from + ', Message: ' + message);
+
+//                var twiml = '<?xml version="1.0" encoding="UTF-8" ?>n<Response>n<Sms>Thanks for your text, well be in touch.</Sms>n</Response>';
+//                console.log(res);
+//                res.send(twiml, {'Content-Type':'text/xml'}, 200);
+//                app.get("http://localhost:4040", function(req,res) {
+//  console.log(req, res);
+// });
+
+// });
 
 app.post('/processtext', function(req,res) {
     if (req.method == 'POST') {
@@ -44,13 +61,12 @@ app.post('/processtext', function(req,res) {
             console.log(POST);
 
             client.messages.create({
-                body: "HEY ALLL!!!!!",
-                to: "+14083869581",
+                body: "FUCK OFF NERD",
+                to: "LOL",
                 from: "+16505420611"
             }, function(err, message) {
                 process.stdout.write(message.sid);
             });
-
 
 
             //validate incoming request is from twilio using your auth token and the header from Twilio
@@ -77,5 +93,4 @@ app.post('/processtext', function(req,res) {
         res.end('send a POST');
     }
   });
-
 
