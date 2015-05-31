96015be307d9b93dbf7977e4f17c229d8180d226
Arash Namvar <namvar.arash@gmail.com>
2015-05-30 21:09:21 -0700

    Josh's #

--- app.js
+++ app.js
@@ -23,28 +23,11 @@ var client = require('twilio')(accountSid, authToken);
  
 client.messages.create({
     body: "Jenny please?! I love you <3",
-    to: "+14083869581",
+    to: "+16502834692",
     from: "+16505420611"
 }, function(err, message) {
     process.stdout.write(message.sid);
 });
-
-// var sys = require('sys');
-
-// app.post('http://f8ec7b1b.ngrok.io', function(req, res) {
-
-// var message = req.body.Body;
-// var from = req.body.From;
-// sys.log('From: ' + from + ', Message: ' + message);
-
-//                var twiml = '<?xml version="1.0" encoding="UTF-8" ?>n<Response>n<Sms>Thanks for your text, well be in touch.</Sms>n</Response>';
-//                console.log(res);
-//                res.send(twiml, {'Content-Type':'text/xml'}, 200);
-//                app.get("http://localhost:4040", function(req,res) {
-// 	console.log(req, res);
-// });
-
-// });
 
 app.post('/processtext', function(req,res) {
     if (req.method == 'POST') {
