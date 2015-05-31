d05630555f5ccd9d56a9da5ed9ce74a9e1ee5eae
Arash Namvar <namvar.arash@gmail.com>
2015-05-31 08:07:01 -0700

    fuck

--- app.js
+++ app.js
@@ -1,122 +1,30 @@
 var express = require("express");
 var path = require("path");
 var app = express();
-var session = require('express-session');
 var bodyParser = require("body-parser");
-var passport = require('passport');
-var uberStrategy = require('passport-uber');
-// var https = require('https');
-// var http = require('http');
-var Uber = require('node-uber');
-var config = require('./config.js');
 var accountSid = 'AC23d38d64f113cbd57fe69b744ae37c46';
 var twilio = require('twilio');
 var qs = require('querystring');
 var authToken = '4767a1a13814d3e80b13773824e79f44';
 var client = require('twilio')(accountSid, authToken);
-var geocodeProvider = 'google';
-var httpAdapter = 'https';
-var extra = {
-    apiKey: "",
-    formatter: null
-};
-var geocoder = require('node-geocoder')(geocodeProvider, httpAdapter, extra);
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "./client")));
-var clientID = config.ClientID;
-var clientSecret = config.ClientSecret;
-var ServerID = config.ServerID;
-var sessionSecret = "UBERAPIROCKS";
-var uber = new Uber({
-  client_id: clientID,
-  client_secret: clientSecret,
-  server_token: ServerID,
-  redirect_uri: "http://localhost:3000/auth/uber/callback",
-  // redirect_uri: "https://uberforall.herokuapp.com/auth/uber/callback",
-  name: 'Textber'
-});
-app.use(session({
-    secret: sessionSecret,
-    resave: false,
-    saveUninitialized: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(bodyParser.urlencoded({extended: true}));
 
 var server = app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
- 
+
+
 client.messages.create({
-    body: "Send me a response, prease",
+    body: "Thank you for signing up for TextBER!",
     to: "+14083869581",
     from: "+16505420611"
 }, function(err, message) {
     process.stdout.write(message.sid);
 });
 
-// var sys = require('sys');
-
-// app.post('http://f8ec7b1b.ngrok.io', function(req, res) {
-
-// var message = req.body.Body;
-// var from = req.body.From;
-// sys.log('From: ' + from + ', Message: ' + message);
-
-//                var twiml = '<?xml version="1.0" encoding="UTF-8" ?>n<Response>n<Sms>Thanks for your text, well be in touch.</Sms>n</Response>';
-//                console.log(res);
-//                res.send(twiml, {'Content-Type':'text/xml'}, 200);
-//                app.get("http://localhost:4040", function(req,res) {
-//  console.log(req, res);
-// });
-
-// });
 
 app.post('/processtext', function(req,res) {
-    if (req.method == 'POST') {
-        var body = '';
-
-        req.on('data', function (data) {
-            body += data;
-        });
-
-        req.on('end', function () {
-            var POST = qs.parse(body);
-            console.log(POST);
-
-            if (POST.From == "+14083869581") {
-                client.messages.create({
-                    body: "Hey, " + POST.From + ". So you want to go to " + POST.Body,
-                    to: "+14083869581",
-                    from: "+16505420611"
-                }, function(err, message) {
-                    process.stdout.write(message.sid);
-                });
-            }
-            var token = '4767a1a13814d3e80b13773824e79f44',
-                header = req.headers['x-twilio-signature'];
-
-            //validateRequest returns true if the request originated from Twilio
-            if (twilio.validateRequest(token, header, 'https://uberforall.herokuapp.com/', POST)) {
-                //generate a TwiML response
-                var resp = new twilio.TwimlResponse();
-                resp.say('hello, twilio!');
-
-                res.writeHead(200, { 'Content-Type':'text/xml' });
-                res.end(resp.toString());
-
-                // PUT JEFF's CODE
-            }
-            else {
-                res.writeHead(403, { 'Content-Type':'text/plain' });
-                res.end('you are not twilio - take a hike.');
-            }
-        });
-    }
-    else {
-        res.writeHead(404, { 'Content-Type':'text/plain' });
-        res.end('send a POST');
-    }
+    console.log(req);    
   });
