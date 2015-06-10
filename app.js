65e2f4a0e0cc7e743e4539eb0c8eabb8d5b02748
Arash Namvar <namvar.arash@gmail.com>
2015-06-10 10:49:16 -0700

    added all sudo logic and stuff

--- app.js
+++ app.js
@@ -1,41 +1,83 @@
+// BASIC DEPENDENCIES
 var express = require("express");
 var path = require("path");
-var app = express();
 var bodyParser = require("body-parser");
-app.use(bodyParser.json());
-
-var twilio = require('twilio');
+var fs = require('fs');
 var qs = require('querystring');
+var session = require('express-session');
+var passport = require('passport');
+var httpAdapter = 'https';
+var https = require('https');
+var http = require('http');
+var config = require('./config.js');
 
+// TWILIO SPECFIC DEPENDENCIES + SETUP 
+var twilio = require('twilio');
+var resp = new twilio.TwimlResponse();
+var accountSid = 'AC23d38d64f113cbd57fe69b744ae37c46';
+var authToken = '4767a1a13814d3e80b13773824e79f44';
+var client = require('twilio')(accountSid, authToken);
+
+// UBER SPECIFIC DEPENDENCIES + SETUP
+var uberStrategy = require('passport-uber');
+var request = require('request');
+var Uber = require('node-uber');
+var clientID = config.ClientID;
+var clientSecret = config.ClientSecret;
+var ServerID = config.ServerID;
+var sessionSecret = "UBERAPIROCKS";
+var globalAccessToken;
+var uber = new Uber({
+  client_id: clientID,
+  client_secret: clientSecret,
+  server_token: ServerID,
+  redirect_uri: "http://localhost:8000/auth/uber/callback",
+  name: 'Textber'
+});
+
+// GOOGLE GEOCODER SETUP
+var geocodeProvider = 'google';
+var extra = {
+    apiKey: "",
+    formatter: null
+};
+var geocoder = require('node-geocoder')(geocodeProvider, httpAdapter, extra);
+
+
+// APP SETUP
+var app = express();
+app.use(session({
+    secret: sessionSecret,
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, "./client")));
-
+app.use(bodyParser.json());
 app.set('views',__dirname + '/views');
 app.set('view engine','ejs');
-
-var count_req = 0;
-
-
-
-// app.set("views", path.join(__dirname, "./views"));
-// app.set("view engine", "ejs");
 
 var server = app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
+// UPON FILLING OUT ONLINE FORM TO SIGNUP FOR TEXTBER {
 
+    //////////////////////////////////////////////////////////////////////////////////////////
+    // SAVE THEIR UBER RELATED INFORMATION AND PHONE NUMBER INTO MONGODB
 
-var accountSid = 'AC23d38d64f113cbd57fe69b744ae37c46';
-var authToken = '4767a1a13814d3e80b13773824e79f44';
-var client = require('twilio')(accountSid, authToken);
- 
-// client.messages.create({
-//     body: "Send me a response, prease",
-//     to: "+14083869581",
-//     from: "+16505420611"
-// }, function(err, message) {
-//     process.stdout.write(message.sid);
-// });
+    // SEND CONFIRMATION EMAIL TO USER
+    // client.messages.create({
+    //     body: "Thank you for signing up for TextBER, please relpy 'accept' to confirm your account",
+    //     to: FORM.PHONE NUMBER
+    //     from: "+16505420611"
+    // }, function(err, message) {
+    //     process.stdout.write(message.sid);
+    // });
+// }
+
 
 var POST;
 app.post('/processcall', function(req,res) {
@@ -47,9 +89,13 @@ app.post('/processcall', function(req,res) {
 
     req.on('end', function () {
 
-      // console.log(body);
         POST = qs.parse(body);
         console.log(POST);
+
+        // LOOK UP THE PHONE NUMBER, IF IT DOESN'T EXIST, HANGUP THEN CALL THEM WITH THE MESSAGE "THE NUMBER YOU CALLED FROM DOESN'T HAVE AN ACCOUNT ASSOCIATED WITH IT
+        // ELSE
+        // RESPOND WITH 'WE'RE PROCESSING YOUR REQUEST, WE'LL CALL BACK ONCE WE'VE BOOKED YOUR RIDE.
+        // LOOKUP THAT USER'S DEFAULT ADDRESS WITH THE NUMBER AND MAKE A REQUEST, CALL BACK WITH THE INFO 
     });
 });
 
@@ -57,86 +103,59 @@ app.post('/processcall', function(req,res) {
 app.post('/processtext', function(req,res) {
     if (req.method == 'POST') {
         var body = '';
-
         req.on('data', function (data) {
             body += data;
         });
-
         req.on('end', function () {
-
-          // console.log(body);
             POST = qs.parse(body);
             console.log(POST);
 
+                 //validate incoming request is from twilio using your auth token and the header from Twilio
+                        header = req.headers['x-twilio-signature'];
 
-            if (POST.From == "+14083869581") {
-                client.messages.create({
-                    body: POST.From + POST.Body,
-                    to: "+14083869581",
-                    from: "+16505420611"
-                }, function(err, message) {
-                    process.stdout.write(message.sid);
-                });
+                    //validateRequest returns true if the request originated from Twilio
+                    if (twilio.validateRequest(authToken, header, 'http://twilio-raw.herokuapp.com', POST)) {
+                        //generate a TwiML response
+                        var resp = new twilio.TwimlResponse();
+                        resp.say('hello, twilio!');
+
+                        res.writeHead(200, { 'Content-Type':'text/xml' });
+                        res.end(resp.toString());
+                    }
+                    else {
+                        res.writeHead(403, { 'Content-Type':'text/plain' });
+                        res.end('you are not twilio - take a hike.');
+                    }
+
+            if (POST.Body == 'accept') {
+                // LOOKUP THE PHONE NUMBER, IF IT MATCHES, CHANGE THE 'CONFIRMED' FIELD IN THE MONGODB TO TRUE FOR THEIR NUMBER, SEND CONFIRMATION
+
+                    // client.messages.create({
+                    //     body: "YOU HAVE NOW BEEN CONFIRMED! GO TO TEXTBER.co/INstructions if you need to learn how to do stuff",
+                    //     to: POST.From
+                    //     from: "+16505420611"
+                    // }, function(err, message) {
+                    //     process.stdout.write(message.sid);
+                    // });
+            } else {
+                for (var x = 0; x < (POST.Body).length; i++ ){
+                    if (POST.Body[i] == ':') {
+
+                        //////////////////////////////////////////////////////////////////////////////////////////
+                        // LOGIC TO LOOK UP PHONE NUMBER, IF IT DOESN'T EXIST, SEND TEXT ABOUT NO ACCOUNT...ELSE {
+                        // USE THAT ACCOUNT INFO THEN LOOK UP THE ADDRESSES AND CONFIRM THAT THEY'RE OKAY, THEN MAKE THE UBER REQUEST, THEN SEND TEXT WITH THE DETAILS
+
+                    } else {
+                        // client.messages.create({
+                        //     body: "INVALID REQUEST, PLEASE CALL US AT 911 TO HELP"
+                        //     to: POST.From
+                        //     from: "+16505420611"
+                        // }, function(err, message) {
+                        //     process.stdout.write(message.sid);
+                        // });
+                    }
+                }
             }
-
-            // if (POST.From == "+15103344759") {
-            //     client.messages.create({
-            //         body: "Hey Uyanga",
-            //         to: "+15103344759",
-            //         from: "+16505420611"
-            //     }, function(err, message) {
-            //         process.stdout.write(message.sid);
-            //     });
-            // }
-
-            // if (POST.From == "+12177214157") {
-            //     client.messages.create({
-            //         body: POST.From + POST.Body,
-            //         to: "+12177214157",
-            //         from: "+16505420611"
-            //     }, function(err, message) {
-            //         process.stdout.write(message.sid);
-            //     });
-            // }
-
-            // if (POST.From == "+19142635538") {
-            //     client.messages.create({
-            //         body: "Hey hefferson, you lil b",
-            //         to: "+19142635538",
-            //         from: "+16505420611"
-            //     }, function(err, message) {
-            //         process.stdout.write(message.sid);
-            //     });
-            // }
-
-            // if (POST.From == "+16502834692") {
-            //     client.messages.create({
-            //         body: POST.From + POST.Body,
-            //         to: "+16502834692",
-            //         from: "+16505420611"
-            //     }, function(err, message) {
-            //         process.stdout.write(message.sid);
-            //     });
-            // }
-            //validate incoming request is from twilio using your auth token and the header from Twilio
-            // var token = '4767a1a13814d3e80b13773824e79f44',
-            //     header = req.headers['x-twilio-signature'];
-
-            // //validateRequest returns true if the request originated from Twilio
-            // if (twilio.validateRequest(token, header, 'https://uberforall.herokuapp.com/', POST)) {
-            //     //generate a TwiML response
-            //     var resp = new twilio.TwimlResponse();
-            //     resp.say('hello, twilio!');
-
-            //     res.writeHead(200, { 'Content-Type':'text/xml' });
-            //     res.end(resp.toString());
-            // }
-            // else {
-            //     res.writeHead(403, { 'Content-Type':'text/plain' });
-            //     res.end('you are not twilio - take a hike.');
-            // }
-
-            count_req = count_req + 1;
         });
     }
     else {
@@ -144,22 +163,3 @@ app.post('/processtext', function(req,res) {
         res.end('send a POST');
     }
   });
-
-    app.get('/results',function(request,response){
-        console.log(POST);
-        
-    var data = [
-            {address: POST.Body,from_data: POST.From, count_req: count_req}
-            ];
-        response.render('results',{data_array: data});
-
-    });
-
-// app.get('/',function(request,response){
-
-//     response.send("<h1>Hello Express</h1>");
-//     console.log(POST);
-    
-// });
-
-
